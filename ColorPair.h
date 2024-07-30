05c40a6188ed2b6311fc3ad094506b227901fd32
RaunakBhattacharya <160215825+RaunakBhattacharya@users.noreply.github.com>
2024-07-30 10:50:45 +0530

    Create ColorPair.h

---
+++ ColorPair.h
@@ -0,0 +1,17 @@
+#include <string>
+#include "MajorColor.h"
+#include "MinorColor.h"
+
+namespace TelCoColorCoder
+{
+    class ColorPair {
+        private:
+            MajorColor majorColor;
+            MinorColor minorColor;
+        public:
+            ColorPair(MajorColor major, MinorColor minor);
+            MajorColor getMajor();
+            MinorColor getMinor();
+            std::string ToString();
+    };
+}
